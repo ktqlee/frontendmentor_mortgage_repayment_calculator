ddfa0d28f2e3bc9bf2af0e0a8e6e6488d70ef937
Quentin Lee <151835338+ktqlee@users.noreply.github.com>
2024-08-02 17:40:01 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -18,11 +18,11 @@ const mortgage_type_interest_label = document.getElementById("mortgage_type_inte
 window.addEventListener("load", (e) => {
     empty_result.style.display = "block";
     completed_result.style.display = "none";
-    set_div_height();
+    set_div_same_height();
 })
 
 // Set the two container to the same height
-function set_div_height(){
+function set_div_same_height(){
     if(calculator_box.offsetHeight != result_box.offsetHeight){
         result_box.style.height = calculator_box.offsetHeight + 'px';
     }
@@ -38,8 +38,7 @@ clear_all.addEventListener("click", (e) => {
     mortgage_type_input[0].checked = mortgage_type_input[1].checked = false;
 
     // Active State for label
-    mortgage_type_repayment_label.style.backgroundColor = "white";
-    mortgage_type_interest_label.style.backgroundColor = "white";
+    reset_label_activestate();
 })
 
 // Active state for mortgage type
@@ -52,13 +51,25 @@ mortgage_type_input[1].addEventListener("click", (e) => {
     mortgage_type_interest_label.style.backgroundColor = "hsl(61, 70%, 52%, 50%)";
 })
 
-// Calculate repayment
+function reset_label_activestate(){
+    mortgage_type_repayment_label.style.backgroundColor = "white";
+    mortgage_type_interest_label.style.backgroundColor = "white";
+}
+
+// Error <p>
+const mortgage_amount_error = document.getElementById("mortgage_amount_error");
+const mortgage_term_error = document.getElementById("mortgage_term_error");
+const interest_rate_error = document.getElementById("interest_rate_error");
+const mortgage_type_error = document.getElementById("mortgage_type_error");
+
 const calculate_button = document.getElementById("calculate_button");
 const form = document.getElementById("form");
 const required_message = "This field is required";
 
+// Calculate repayment
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
+    e.preventDefault();show_result();
+
     if(validate()){
         calculate();
         show_result();
@@ -66,23 +77,51 @@ form.addEventListener("submit", (e) => {
 })
 
 function validate(){
+    let valid = true;
+    reset_error();
+    if(mortgage_amount.value == ""){
+        mortgage_amount_error.innerHTML = required_message;
+        valid = false;
+    }
+    if(mortgage_term.value == ""){
+        mortgage_term_error.innerHTML = required_message;
+        valid = false;
+    }
+    if(interest_rate.value == ""){
+        interest_rate_error.innerHTML = required_message;
+        valid = false;
+    }
+    if(!mortgage_type_input[0].checked && !mortgage_type_input[1].checked){
+        mortgage_type_error.innerHTML = required_message;
+        valid = false;
+    }
+    set_div_same_height();
+    return valid;
+}
 
+function reset_error(){
+    mortgage_amount_error.innerHTML = mortgage_term_error.innerHTML =
+    interest_rate_error.innerHTML = mortgage_type_error.innerHTML = "";
 }
 
 function calculate(){
     console.log("calculate");
+    // Repayment type
+    let P = mortgage_amount.value;
+    let r = (interest_rate / 100) / 12;
+    let n = mortgage_term.value * 12;
+
+    // equation = Pr / ( 1 - ( 1 / (1-r)^n ))
+    let monthly_repay = ( P * r ) / ( 1 - ( 1 / Math.pow((1-r), n) ) );
+    let total_repay = monthly_repay * n;
 }
 
 function show_result(){
-
+    empty_result.style.display = "none";
+    completed_result.style.display = "block";
 }
 
-const mortgage_amount_error = document.getElementById("mortgage_amount_error");
-const mortgage_term_error = document.getElementById("mortgage_term_error");
-const interest_rate_error = document.getElementById("interest_rate_error");
-const mortgage_type_error = document.getElementById("mortgage_type_error");
 
-mortgage_amount_error.innerHTML = required_message;
-// mortgage_term_error.innerHTML = required_message;
-interest_rate_error.innerHTML = required_message;
-mortgage_type_error.innerHTML = required_message;
+
+
+
